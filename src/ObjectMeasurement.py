447f6097f7f5319d8ceac2b6305014cd782ab653
Drivas <drivaspedro@gmail.com>
2021-10-11 00:53:07 -0300

    feature - calculate the object size

--- src/ObjectMeasurement.py
+++ src/ObjectMeasurement.py
@@ -6,19 +6,52 @@ BRIGHTNESS = 10
 WIDTH = 3
 HEIGHT = 4
 
-webcam = False
+webcam = True
 path = '../assets/1.jpg'
 cap = cv2.VideoCapture(0)
-cap.set(BRIGHTNESS,55) 
+cap.set(BRIGHTNESS,50) 
 cap.set(WIDTH,1280)
 cap.set(HEIGHT,720)
+scale = 3
+paperWidth = 210 * scale
+paperHeight = 297 * scale
+
 
 while True:
     
     if webcam:success,img = cap.read()
     else: img = cv2.imread(path)
     
-    img , finalContours = utils.getContours(img,showCanny=True,draw=True)
+    img , contours = utils.getContours(img,minArea=50000,filter=4)
+    
+    if len(contours) != 0:
+        biggest = contours[0][2]
+        #print(biggest)
+        imgWarp = utils.warpImg(img,biggest,paperWidth,paperHeight)
+        
+        #IMAGEM COM OS CONTORNOS A SEREM MEDIDOS
+        img2 , contours2 = utils.getContours(imgWarp,minArea=2000,filter=4,threshold=[50,50],draw=True)
+        
+        if len(contours) != 0:
+            for obj in contours2:
+                cv2.polylines(img2,[obj[2]],True,(0,255,0),2)
+                newPoints = utils.reorder(obj[2])
+                # ACHANDO AS DISTANCIAS ESTRE UM OBJETO RETANGULAR
+                newW = round( utils.findDistance(newPoints[0][0]//scale,nPoints[1][0]//scale),1) #Dividir pelo scale pra ficar correto )
+                newH = round( utils.findDistance(newPoints[0][0]//scale,nPoints[2][0]//scale),1) #Dividir pelo scale pra ficar correto )
+                
+                cv2.arrowedLine(imgContours2, (nPoints[0][0][0], nPoints[0][0][1]), (nPoints[1][0][0], nPoints[1][0][1]),
+                                (255, 0, 255), 3, 8, 0, 0.05)
+                cv2.arrowedLine(imgContours2, (nPoints[0][0][0], nPoints[0][0][1]), (nPoints[2][0][0], nPoints[2][0][1]),
+                                (255, 0, 255), 3, 8, 0, 0.05)
+                x, y, w, h = obj[3]
+                cv2.putText(imgContours2, '{}cm'.format(nW), (x + 30, y - 10), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1.5,
+                            (255, 0, 255), 2)
+                cv2.putText(imgContours2, '{}cm'.format(nH), (x - 70, y + h // 2), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1.5,
+                            (255, 0, 255), 2)
+
+        cv2.imshow('A4',img2)
+
     
     img = cv2.resize(img,(0,0),None,0.4,0.4)
     cv2.imshow('Original',img)
--- src/utils.py
+++ src/utils.py
@@ -32,4 +32,33 @@ def getContours(img,threshold=[100,100],showCanny=False,minArea=1000,filter=0,dr
             cv2.drawContours(img,contours[4],-1,(0,0,255),3)
     
     return img,finalContours
+
+def reorder(myPoints):
+    print(myPoints.shape)
+    myPointsNew = np.zeros_like(myPoints)
+    myPoints = myPoints.reshape((4,2))
+    add = myPoints.sum(1)
+    myPointsNew[0] = myPoints[np.argmin(add)] #PONTO 1 do papel
+    myPointsNew[3] = myPoints[np.argmax(add)] #PONTO 4 do papel
+    diff = np.diff(myPoints,axis=1)
+    myPointsNew[1] = myPoints[np.argmin(diff)]
+    myPointsNew[2] = myPoints[np.argmax(diff)]
+    return myPointsNew
+    
+
+def warpImg (img,points,widht,height,pad=20):
+    #print(points)
+    points = reorder(points)
+    
+    pts1 = np.float32(points)
+    pts2 = np.float32([[0,0],[widht,0],[0,height],[widht,height]])
+    matrix = cv2.getPerspectiveTransform(pts1,pts2)
+    imgWarp = cv2.warpPerspective(img,matrix,(widht,height))
+    imgWarp = imgWarp[pad:imgWarp.shape[0]-pad,pad:imgWarp.shape[1]-pad] #Remove as bordas da imagem fora do A4
+    
+    return imgWarp
+
+def findDistance(pts1,pts2):
+    return ((pts2[0]-pts1[0])^2 + (pts2[1]-pts1[1])^2)^0.5 #Acha a distancia entre dois pontos pelo teorema de pitagoras
+    
             
